99e0412b5c856740595e0f230c23902e69bc7ac7
Ahmed <Ahmed@DESKTOP-BK44UB9>
2022-02-24 09:16:02 +0100

    ahmed

--- Rest_App/src/main/java/tn/esprit/spring/entity/Commentaire.java
+++ Rest_App/src/main/java/tn/esprit/spring/entity/Commentaire.java
@@ -1,5 +1,27 @@
 package tn.esprit.spring.entity;
 
-public class Commentaire {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+
+@Entity
+public class Commentaire implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	@JsonIgnore
+	private Publication publication;
+	
 
 }
--- Rest_App/src/main/java/tn/esprit/spring/entity/Departement.java
+++ Rest_App/src/main/java/tn/esprit/spring/entity/Departement.java
@@ -1,5 +1,27 @@
 package tn.esprit.spring.entity;
 
-public class Departement {
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+@Entity
+public class Departement  implements Serializable {
+	
+
+		private static final long serialVersionUID = 1L;
+		@Id
+		@GeneratedValue(strategy=GenerationType.IDENTITY)
+		private Long id;
+		private String name; 
+		@OneToMany(mappedBy="departement", 
+				cascade={CascadeType.PERSIST, CascadeType.REMOVE},
+				fetch=FetchType.EAGER)
+		private List<Publication> publication;
 
 }
