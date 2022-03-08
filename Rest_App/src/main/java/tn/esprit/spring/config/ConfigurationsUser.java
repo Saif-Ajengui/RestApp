752b921262aaf6f893b5b7337cc62b9585837fcf
Ajengui <Ajengui@DESKTOP-2G4M36G>
2022-03-08 14:31:59 +0100

    TCHAT

--- Rest_App/src/main/java/tn/esprit/spring/config/ConfigurationsUser.java
+++ Rest_App/src/main/java/tn/esprit/spring/config/ConfigurationsUser.java
@@ -42,20 +42,24 @@ public class ConfigurationsUser extends WebSecurityConfigurerAdapter  {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
+		
+		
 				http.csrf().disable()
-				.authorizeRequests().antMatchers("/Authentification/**").permitAll().
-				
+				.authorizeRequests() 
+				.antMatchers("/Authentification/**").permitAll()
+				 .antMatchers("/**/**").permitAll().
 				// all other requests need to be authenticated
 				anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
 				// store user's state.
 				exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
+			
+				
+				
 		// Add a filter to validate the tokens with every request
 		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-	
-				
+		
 	}
 	
 	
---
+++ Rest_App/src/main/java/tn/esprit/spring/controller/ChatController.java
@@ -0,0 +1,30 @@
+package tn.esprit.spring.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.web.bind.annotation.RestController;
+
+import tn.esprit.spring.response.ChatMessage;
+
+
+@RestController
+public class ChatController {
+
+	@MessageMapping("/chat.register")
+	@SendTo("/topic/public")
+	public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+		
+		
+		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+		return chatMessage;
+	}
+
+	@MessageMapping("/chat.send")
+	@SendTo("/topic/public")
+	public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+		return chatMessage;
+	}
+
+}
